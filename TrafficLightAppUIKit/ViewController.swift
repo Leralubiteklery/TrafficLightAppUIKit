0a0b54ed89e8794655fa50a29f6b0aad008e7d6f
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-01-21 13:30:26 +0300

    Initial Commit

---
+++ TrafficLightAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightAppUIKit
+//
+//  Created by Lera Savchenko on 21.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
